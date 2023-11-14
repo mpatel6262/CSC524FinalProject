c83c14cc4d94e676593d145c862946ad121d705a
mpatel6262 <pmaanav9753@gmail.com>
2023-11-14 10:47:42 -0800

    Added organization of ctfs into individual modules, tested with ctf 1

---
+++ CTFScripts/ctf_1.py
@@ -0,0 +1,31 @@
+import time
+
+def CTF_1(client_socket):
+    message = "Enter Ground Station Location ID:"
+    client_socket.send(message.encode())
+    while True:
+        # Establish a connection
+        message = client_socket.recv(1024).decode()
+        print(f"Received message: {message}")
+        time.sleep(5)
+        if message == "14-214":
+            response = "Ground Station: Valid ID Provided, Enter your Password:"
+            client_socket.send(response.encode())
+            break
+        
+        response = "Ground Station: Incorrect ground station location ID: Try again."
+        client_socket.send(response.encode())
+
+
+    while True:
+        # Establish a connection
+        message = client_socket.recv(1024).decode()
+        print(f"Received message: {message}")
+        time.sleep(5)
+        if message == "twosigma":
+            response = "\nGround Station: Valid Password!\nWelcome Stephen Beard!\nReminder: You will need your PhD dissertation title on hand for the next stage!\n\nEnter Authentication Step:"
+            client_socket.send(response.encode())
+            break
+        
+        response = "Ground Station: Incorrect Password: Try Again."
+        client_socket.send(response.encode())
---
+++ CTFScripts/ctf_2.py
@@ -0,0 +1,5 @@
+title = "TRUSTGUARD: A MODEL FOR PRACTICAL TRUST IN REAL SYSTEMS"
+
+message = "TRUSTGUARD: A MODEL fOR PRACTICAL tRUST IN REAL SYSTEMs"
+if message.casefold() == title.casefold():
+    print("Pass")
